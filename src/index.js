b7e06af530edeef58ad362ce0e58664e47a64143
Natalia Derbeneva <nat_ff@mail.ru>
2020-03-17 08:15:45 +0300

    init: start 'reverse-int' task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function reverse (n) {
+    let str = (Math.abs(n)).toString();
+    let newstr = "";
+
+    str = str.replace(/0+$/,"");
+   
+    for(let i=str.length-1; i>-1; i--){
+        newstr += str[i];
+    }
   
+    return Number(newstr);
 }
